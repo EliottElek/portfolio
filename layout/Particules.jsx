e7519a13cd4d18d126b909254ffe9c999758f605
Eliott Morcillo <eliott.morcillo@gmail.com>
2022-08-05 12:43:21 +0200

    feat: new particles style

--- layout/Particules.jsx
+++ layout/Particules.jsx
@@ -1,85 +1,125 @@
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-
-const options = {
-  background: {
-    color: {
-      value: "transparent",
-    },
-  },
-  fpsLimit: 150,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: true,
-        mode: "push",
-      },
-      onHover: {
-        enable: false,
-        mode: "repulse",
-      },
-      resize: true,
-    },
-    modes: {
-      push: {
-        quantity: 4,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: "#21c55d",
-    },
-    links: {
-      color: "#21c55d",
-      distance: 150,
-      enable: true,
-      opacity: 0.7,
-      width: 0.5,
-    },
-    collisions: {
-      enable: true,
-    },
-    move: {
-      directions: "none",
-      enable: true,
-      outModes: {
-        default: "bounce",
-      },
-      random: false,
-      speed: 3,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 1000,
-      },
-      value: 80,
-    },
-    opacity: {
-      value: 0.7,
-    },
-    shape: {
-      type: "circle",
-    },
-    size: {
-      value: { min: 2, max: 4 },
-    },
-  },
-  detectRetina: true,
-};
-
 const Page = () => {
   const particlesInit = async (main) => {
     await loadFull(main);
   };
-
-  return <Particles className="z-0" init={particlesInit} options={options} />;
+  return (
+    <Particles
+      className="z-0"
+      init={particlesInit}
+      options={{
+        particles: {
+          number: {
+            value: 355,
+            density: {
+              enable: true,
+              value_area: 789.1476416322727,
+            },
+          },
+          color: {
+            value: "#21c55d",
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000",
+            },
+            polygon: {
+              nb_sides: 5,
+            },
+            image: {
+              src: "img/github.svg",
+              width: 100,
+              height: 100,
+            },
+          },
+          opacity: {
+            value: 0.48927153781200905,
+            random: false,
+            anim: {
+              enable: true,
+              speed: 0.9,
+              opacity_min: 0,
+              sync: false,
+            },
+          },
+          size: {
+            value: 2,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 2,
+              size_min: 0,
+              sync: false,
+            },
+          },
+          line_linked: {
+            enable: false,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200,
+            },
+          },
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: true,
+              mode: "bubble",
+            },
+            onclick: {
+              enable: true,
+              mode: "push",
+            },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 83.91608391608392,
+              size: 1,
+              duration: 3,
+              opacity: 1,
+              speed: 3,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
+            },
+          },
+        },
+        retina_detect: true,
+      }}
+    />
+  );
 };
 
 export default Page;
